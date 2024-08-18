217a8a20da8644b03c5ad862ab2150aa971a3913
Adfmu <Adfmu3@gmail.com>
2024-08-18 13:36:58 +0200

    create book constructor

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+const myLibrary = [];
+
+function Book(title, author, pages, wasRed) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.wasRed = wasRed;
+}
+
+function addBOokToLibrary() {
+    
+}
