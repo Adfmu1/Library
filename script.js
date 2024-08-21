0833bc2ce90f4c092c63709ec4b513e88787753b
Adfmu <Adfmu3@gmail.com>
2024-08-21 14:00:56 +0200

    create get form values from inputs, fix Book synthax

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ const submitButton = document.querySelector("#add-book-btn").addEventListener(
     }
 )
 
+// getting book title, author, pages and was read
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const wasRead = document.querySelector("#was-read");
+
 const myLibrary = [];
 
 function Book(title, author, pages, wasRed) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
     this.wasRed = wasRed;
 }
 
